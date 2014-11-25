d30532efba18ea3140f6db127efbe9463e03f51d
PotatoBoy10 <jerik_is_cool@yahoo.com>
2014-11-25 22:02:05 +0100

    Added build.gradle and first mod class Removed eclipse folder form being tracted

---
+++ src/main/java/com/pandahat/chemengi/ChemicalEngineering.java
@@ -0,0 +1,24 @@
+package com.pandahat.chemengi;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ChemicalEngineering", name="Chemical Engineering", version="1.7.10-1.0")
+public class ChemicalEngineering {
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLInitializationEvent event){
+		
+	}
+}
